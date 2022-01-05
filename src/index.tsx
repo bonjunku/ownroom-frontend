e90d5cb6cc956093608664d6d7a4b80b093a0a09
bonjunku <ku96kbj@naver.com>
2022-01-05 21:47:06 +0900

    :lipstick: design : grid 배치, Navbar 배치

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,68 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GlobalStyle } from './styles/global';
 
 import App from './App';
+import styled from 'styled-components';
+
+const StyledNavbar = styled.nav`
+  height: 80px;
+  background-color: blue;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+const Gridbox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  position: fixed;
+  height: 100%;
+  width: 1680px;
+  opacity: 15%;
+`;
+const Grid = styled.div`
+  background-color: red;
+  width: 189px;
+  height: 100%;
+`;
+const InnerContainer = styled.div`
+  width: 1680px;
+  height: 100%;
+  background-color: white;
+`;
+
+const StyledImg = styled.img`
+  height: 100%;
+`;
+const Navbar = () => {
+  return (
+    <StyledNavbar>
+      <InnerContainer>
+        <StyledImg src={process.env.PUBLIC_URL + 'img/home/logo.png'} />
+      </InnerContainer>
+    </StyledNavbar>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <Gridbox>
+        <Grid />
+        <Grid />
+        <Grid />
+        <Grid />
+        <Grid />
+        <Grid />
+        <Grid />
+        <Grid />
+      </Gridbox>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -2,13 +2,24 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   :root{
-
+    background-color: yellow;
+  
   }
 
   *{
     margin:0;
-    box-sizing:border-box;
-    background-color: yellow;
-    
+    padding:0;
+    box-sizing:border-box; 
+  }
+  html,body{
+    width:100%;
+    height: 100%;
+  }
+  #root{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 `;
