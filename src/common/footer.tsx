6ac6472646b2be2f28c90bbdf27b325a6126c796
bonjunku <ku96kbj@naver.com>
2022-01-30 09:49:26 +0900

    FEAT : 푸터 링크 추가

--- src/common/footer.tsx
+++ src/common/footer.tsx
@@ -5,9 +5,23 @@ import { Text } from '../common/text';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import { icon_insta, logo_footer } from '../static/images/svg';
+import { useAppSelect } from '../store/configureStore.hooks';
+import { getUserInfo } from '../store/modules/user';
 
 export const Footer = () => {
   const navigate = useNavigate();
+  const { isLoggedIn } = useAppSelect(getUserInfo);
+  const handleClickConsultantPortfolio = () => {
+    navigate('/consultantPortfolio');
+  };
+  const handleClickConsultantApplication = () => {
+    if (!isLoggedIn) {
+      alert('로그인 후 이용이 가능한 서비스입니다.');
+      navigate('/login');
+    } else {
+      navigate('/consultant/application');
+    }
+  };
 
   return (
     <Container height="300px" style={FooterCSS}>
@@ -29,22 +43,31 @@ export const Footer = () => {
           컨설턴트
         </Text>
 
-        <Text style={FooterConsultantMenuCSS}>
+        <Text
+          style={FooterConsultantMenu1CSS}
+          onClick={handleClickConsultantPortfolio}
+        >
           포트폴리오 둘러보기
-          <br />
+        </Text>
+        <Text
+          style={FooterConsultantMenu2CSS}
+          onClick={handleClickConsultantApplication}
+        >
           컨설턴트 신청하기
         </Text>
 
         <Text className="KRHeadline-2 gray002" style={FooterFormDownloadCSS}>
           양식 다운받기
         </Text>
-        <Text style={FooterFormDownloadMenuCSS}>
-          컨설턴트 신청서
-          <br />
-          컨설팅 신청서
-          <br />
-          컨설팅 보고서
-        </Text>
+        <a href="/format/온룸 컨설턴트 신청서 양식.docx" download>
+          <Text style={FooterFormDownloadMenu1CSS}>컨설턴트 신청서</Text>
+        </a>
+        <a href="/format/온룸 컨설팅 신청서 양식.docx" download>
+          <Text style={FooterFormDownloadMenu2CSS}>컨설팅 신청서</Text>
+        </a>
+        <a href="/format/온룸 컨설팅 보고서 양식.docx" download>
+          <Text style={FooterFormDownloadMenu3CSS}>컨설팅 보고서</Text>
+        </a>
         <Img
           src={icon_insta}
           width="18px"
@@ -52,9 +75,11 @@ export const Footer = () => {
           style={FooterInstaIconCSS}
         ></Img>
         <FooterInstaLink></FooterInstaLink>
-        <Text className="KRBody-2 orange001" style={FooterInstaLinkCSS}>
-          ownroom.official
-        </Text>
+        <a href="https://www.instagram.com/ownroom.official/">
+          <Text className="KRBody-2 orange001" style={FooterInstaLinkCSS}>
+            ownroom.official
+          </Text>
+        </a>
       </Container>
     </Container>
   );
@@ -81,10 +106,22 @@ const FooterConsultantCSS: CSSProperties = {
   top: '56px',
   fontSize: '15px',
 };
-const FooterConsultantMenuCSS: CSSProperties = {
+const FooterConsultantMenu1CSS: CSSProperties = {
+  cursor: 'pointer',
   position: 'absolute',
   left: '290px',
   top: '102px',
+  fontFamily: 'Noto Sans KR',
+  lineHeight: '30px',
+  color: 'var(--gray-gray-002)',
+  fontSize: '15px',
+};
+
+const FooterConsultantMenu2CSS: CSSProperties = {
+  cursor: 'pointer',
+  position: 'absolute',
+  left: '290px',
+  top: '132px',
   fontFamily: 'Noto Sans KR',
   lineHeight: '30px',
   color: 'var(--gray-gray-002)',
@@ -98,9 +135,31 @@ const FooterFormDownloadCSS: CSSProperties = {
   fontSize: '15px',
 };
 
-const FooterFormDownloadMenuCSS: CSSProperties = {
+const FooterFormDownloadMenu1CSS: CSSProperties = {
+  cursor: 'pointer',
   position: 'absolute',
   top: '102px',
+  left: '483px',
+  fontFamily: 'Noto Sans KR',
+  lineHeight: '30px',
+  color: 'var(--gray-gray-002)',
+  fontSize: '15px',
+};
+const FooterFormDownloadMenu2CSS: CSSProperties = {
+  cursor: 'pointer',
+  position: 'absolute',
+  top: '132px',
+  left: '483px',
+  fontFamily: 'Noto Sans KR',
+  lineHeight: '30px',
+  color: 'var(--gray-gray-002)',
+  fontSize: '15px',
+};
+
+const FooterFormDownloadMenu3CSS: CSSProperties = {
+  cursor: 'pointer',
+  position: 'absolute',
+  top: '162px',
   left: '483px',
   fontFamily: 'Noto Sans KR',
   lineHeight: '30px',
