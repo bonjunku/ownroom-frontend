d2dd9486d17f2daa7baba6cd034f63bafba0dbdb
Bonjun Ku <ku96kbj@naver.com>
2022-06-23 18:15:43 +0900

    fix : Row 컴포넌트 수정

--- src/common/Row.tsx
+++ src/common/Row.tsx
@@ -1,13 +1,18 @@
-import styled from "styled-components"
+import styled, { css, CSSProperties } from "styled-components"
 
 interface RowProps {
     height : number;
     mainAxisAlignment? : 'center'|'start'|'end'|'flex-start'|'flex-end'|'left'|'right';
     children : JSX.Element | JSX.Element[];
+    style? : CSSProperties
 }
 
-export const Row = ({height, children,mainAxisAlignment='start'}: RowProps) =>{
-    return (<Container height={height} mainAxisAlignment={mainAxisAlignment}>{children}</Container>)
+export const Row = ({height, children,mainAxisAlignment='start', style=defaultStyle}: RowProps) =>{
+    return (<Container height={height} mainAxisAlignment={mainAxisAlignment} style={style}>{children}</Container>)
+}
+
+const defaultStyle : CSSProperties={
+
 }
 
 const Container = styled.div<RowProps>`
@@ -15,6 +20,6 @@ const Container = styled.div<RowProps>`
     display:flex;
     flex-direction:row;
     justify-content : ${({mainAxisAlignment})=>mainAxisAlignment};
-    algin-items: center;
+    align-items: center;
     width : 100%;
 `
