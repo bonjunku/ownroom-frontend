9e6cf2deecadde80221d36a63f75dfb02c4ff897
Bonjun Ku <ku96kbj@naver.com>
2022-06-23 20:31:48 +0900

    chore : Prettier 적용

--- src/common/Spacing.tsx
+++ src/common/Spacing.tsx
@@ -1,17 +1,15 @@
 import styled from "styled-components";
 
 interface SpacingProps {
-    width?: number;
-    height?: number;
+  width?: number;
+  height?: number;
 }
 
-export const Spacing = ({width=100,height=100}:SpacingProps)=>{
-return <Container width={width} height={height}></Container>
-}
+export const Spacing = ({ width = 100, height = 100 }: SpacingProps) => {
+  return <Container width={width} height={height}></Container>;
+};
 
 const Container = styled.div<SpacingProps>`
-width: ${({width})=>width}px;
-height: ${({height})=>height}px;
-`
-
-
+  width: ${({ width }) => width}px;
+  height: ${({ height }) => height}px;
+`;
--- src/common/text.tsx
+++ src/common/text.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Text = styled.p`
   display: inline-block;
