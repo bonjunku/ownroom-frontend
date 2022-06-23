d61c7990cbf3ddbf10ea45b17968667d664edc9c
Bonjun Ku <ku96kbj@naver.com>
2022-06-23 20:05:44 +0900

    fix : text 컴포넌트 rename, 수정

--- src/common/text.tsx
+++ src/common/text.tsx
@@ -1,9 +1,5 @@
 import styled from 'styled-components';
 
-interface IText extends React.HTMLAttributes<HTMLElement> {
-  type: 'KRDisplay-1';
-}
-
-export const Text = styled.span`
+export const Text = styled.p`
   display: inline-block;
 `;
