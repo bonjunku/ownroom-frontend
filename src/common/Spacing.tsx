9b8b840de4eff65c03bece5a63526bb629f82126
Bonjun Ku <ku96kbj@naver.com>
2022-06-25 08:53:32 +0900

    fix : Spacing 컴포넌트 100%되도록 수정

--- src/common/Spacing.tsx
+++ src/common/Spacing.tsx
@@ -1,15 +1,36 @@
-import styled from "styled-components";
+import styled, { css } from 'styled-components';
 
 interface SpacingProps {
   width?: number;
   height?: number;
 }
 
-export const Spacing = ({ width = 100, height = 100 }: SpacingProps) => {
+export const Spacing = ({ width, height }: SpacingProps) => {
   return <Container width={width} height={height}></Container>;
 };
 
 const Container = styled.div<SpacingProps>`
-  width: ${({ width }) => width}px;
-  height: ${({ height }) => height}px;
+  ${({ width }) => {
+    if (width) {
+      return css`
+        width: ${width}px;
+      `;
+    } else {
+      return css`
+        width: 100%;
+      `;
+    }
+  }}
+
+  ${({ height }) => {
+    if (height) {
+      return css`
+        height: ${height}px;
+      `;
+    } else {
+      return css`
+        height: 100%;
+      `;
+    }
+  }}
 `;
